0ae8351927eb0d19bfdd255b58eaf063a5b655df
Enmanuel Enrique Bonilla Melara <67129217+Ebonilla1808@users.noreply.github.com>
2023-11-19 19:01:06 -0600

    first

---
+++ Parcial4_BM/Ventas.swift
@@ -0,0 +1,20 @@
+//
+//  Ventas.swift
+//  Parcial4_BM
+//
+//  Created by MacOsX on 11/19/23.
+//
+
+import SwiftUI
+
+struct Ventas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Ventas_Previews: PreviewProvider {
+    static var previews: some View {
+        Ventas()
+    }
+}
